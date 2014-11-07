3ab51b38709cb6d1b7646a18bd496bb66426f6b9
Michael Grubb <michael@grubb.co>
2014-11-06 19:39:20 -0600

    Initial Commit

---
+++ UnixTime/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UnixTime
+//
+//  Created by Michael Grubb on 11/6/14.
+//  Copyright (c) 2014 Michael Grubb. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ UnixTime/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  UnixTime
+//
+//  Created by Michael Grubb on 11/6/14.
+//  Copyright (c) 2014 Michael Grubb. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
